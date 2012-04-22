b44524f9ff0da1027b378f91ae7686f37ae6fab2
Sean Eshbaugh <seaneshbaugh@gmail.com>
2012-04-21 20:49:20 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daysee::Application.config.secret_token = '82cdfc92f92ad3bb505c1a8d0d0eeacd0e911b2e825d490345f8d30fd97f968c2e7752dbefcd0b63a4a0bcaea50447a952f60d93f54bb6c2b538c1d99bd86ec1'
